b1ed4049da0e1cfd368e18ede7212d7d27f59e5b
unknown <dan@mdlindsey.com>
2021-05-27 00:32:22 -0400

    API url

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -1,4 +1,4 @@
-window.apiUrl = 'http://localhost:8080'
+window.apiUrl = 'https://v1.callofdutyapi.com' //'http://localhost:8080'
 // Equivalent of jQuery .ready
 document.addEventListener('DOMContentLoaded',function(){
 
